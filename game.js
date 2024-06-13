a6df0891b24bbe76a3900f2604d8b907366a041b
Alevtina Razumovskaia <148491912+ally-razum@users.noreply.github.com>
2024-06-13 23:47:32 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -4,5 +4,18 @@ let ink = 0
 button.addEventListener("click", (event) => {
     event.preventDefault()
        ink += 1
-       scores.value = ink
+    if (ink === 25){
+        scores.value = '25 очков! 1 цилиндр'
+    } else if (ink === 50) {
+        scores.value = '50 очков! 2 цилиндр'
+    } else if (ink === 75) {
+        scores.value = '75 очков! 3 цилиндр'
+    } else if (ink === 100) {
+        scores.value = '100 очков! 4 цилиндр'
+    } else {
+        scores.value = ink
+    }
+
 })
+
+
